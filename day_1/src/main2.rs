2580d42fe80f9aeaac3505efbd45c54c8053bf9e
MajorX234 <majorx234@googlemail.com>
2023-12-06 00:12:08 +0100

    Add functions to find first and last number in string

--- day_1/src/main2.rs
+++ day_1/src/main2.rs
@@ -1,9 +1,37 @@
 use aoc_lib::read_arg_file;
 use std::io::{self, prelude::*, BufReader};
 
+fn find_last_numeric_in_string(x: &String) -> Option<u8> {
+    let substring_list = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine",
+    ];
+    let mut best_pos = 0;
+    let mut best_string = "0";
+    for substring in substring_list {
+        if let Some(start) = x.rfind(substring) {
+            if start >= best_pos {
+                best_string = substring;
+                best_pos = start;
+            }
+        }
+    }
+    let best_digit_string = best_string
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9");
+    return Some(best_digit_string.chars().last().unwrap() as u8 - '0' as u8);
+}
+
 fn find_first_numeric_in_string(x: &String) -> Option<u8> {
     let substring_list = [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "tow", "three", "four", "five", "six",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
         "seven", "eight", "nine",
     ];
     let mut best_pos = x.len() + 1;
@@ -38,12 +66,16 @@ fn main() {
             let mut last: Option<u8> = None;
 
             first = find_first_numeric_in_string(&x);
-            let x_reverse = x.chars().rev().collect::<String>();
-            last = find_first_numeric_in_string(&x_reverse);
-
-            println!("x: {} first: {} last: {}", x, first.unwrap(), last.unwrap());
+            last = find_last_numeric_in_string(&x);
             acc += first.unwrap() as u32 * 10 + last.unwrap() as u32;
-        }
+            println!(
+                "x: {} first: {} last: {} acc: {}",
+                x,
+                first.unwrap(),
+                last.unwrap(),
+                acc
+            );
+        };
         acc
     };
     let sum = reader.lines().fold(sum, fold_fct_task);
